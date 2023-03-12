9618c3308623e6abe1997d8bd12ea0763241c631
karljanar <karljanar@pop-os.localdomain>
2023-03-12 16:18:33 +0200

    4.2

---
+++ 4.2/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter41/screens/chats.dart';
+import 'package:flutter41/screens/profile.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        appBarTheme: AppBarTheme(
+          titleTextStyle: Theme.of(context)
+              .textTheme
+              .apply(
+                bodyColor: Colors.green.shade50,
+                displayColor: Colors.green.shade50,
+              )
+              .headline6,
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Colors.green,
+          selectedItemColor: Colors.green.shade50,
+          unselectedItemColor: Colors.green.shade900,
+        ),
+        scaffoldBackgroundColor: Colors.green.shade100,
+        textTheme: Theme.of(context).textTheme.apply(
+          bodyColor: Colors.green.shade50,
+          displayColor: Colors.green.shade50,
+        ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int pageIndex = 0;
+  final pages = [const ChatsScreen(), const ProfileScreen()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            centerTitle: true,
+            title: Text(pageIndex == 0 ? "Chats" : "Profile")),
+        body: pages[pageIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: pageIndex,
+          enableFeedback: false,
+          onTap: (int selection) {
+            setState(() {
+              pageIndex = selection;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chats"),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile",
+            )
+          ],
+        ));
+  }
+}
---
+++ 4.2/lib/screens/chats.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../models/chats.dart';
+import '../widgets/chatsList.dart';
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({Key? key}) : super(key: key);
+
+  @override
+  ChatsScreenState createState() => ChatsScreenState();
+}
+
+class ChatsScreenState extends State<ChatsScreen> {
+  final List<Chats> _chatList = [
+    Chats(
+        name: "User1",
+        message: "Lorem ipsum dolor sit amet",
+        time: "12:30",
+        seenMessage: true),
+    Chats(
+        name: "User2",
+        message:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        time: "12:30",
+        seenMessage: false),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+                shrinkWrap: true,
+                physics: const ScrollPhysics(),
+                itemCount: _chatList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ChatsList(chat: _chatList[index]);
+
+                })
+          ],
+        ));
+
+  }
+}

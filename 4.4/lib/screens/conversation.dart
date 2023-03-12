f13cc5144b04c105f2d43f7e540f0b0d180b815e
karljanar <karljanar@pop-os.localdomain>
2023-03-12 15:49:27 +0200

    4.4

---
+++ 4.4/lib/screens/conversation.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/models/conversation.dart';
+
+import '../widgets/conversationList.dart';
+
+class ConversationScreen extends StatefulWidget {
+  final String name;
+
+  const ConversationScreen({Key? key, required this.name}) : super(key: key);
+
+  @override
+  ConversationScreenState createState() => ConversationScreenState();
+}
+
+class ConversationScreenState extends State<ConversationScreen> {
+  late List<Conversation> conversationList;
+
+  @override
+  void initState() {
+    conversationList = [
+      Conversation(
+          message:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
+          time: DateTime.now(),
+          messageType: MessageType.text,
+          from: widget.name),
+      Conversation(
+          message: "Lorem ipsum dolor sit",
+          time: DateTime.now(),
+          messageType: MessageType.text,
+          from: 'You'),
+      Conversation(
+          time: DateTime.now(),
+          messageType: MessageType.media,
+          mediaLocation: MediaLocation.network,
+          fileName: "https://picsum.photos/400/500",
+          from: "You"),
+      Conversation(
+          time: DateTime.now(),
+          messageType: MessageType.media,
+          mediaLocation: MediaLocation.network,
+          fileName: "https://picsum.p",
+          from: 'You'),
+    ];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.name),
+      ),
+      body: ListView.builder(
+          shrinkWrap: true,
+          physics: const ScrollPhysics(),
+          itemCount: conversationList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ConversationList(
+              conversation: conversationList[index],
+            );
+          }),
+    );
+  }
+}
---
+++ 4.4/lib/widgets/conversationList.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+
+import 'package:date_format/date_format.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../models/conversation.dart';
+
+class ConversationList extends StatefulWidget {
+  Conversation conversation;
+
+  ConversationList({Key? key, required this.conversation}) : super(key: key);
+
+  @override
+  ConversationListState createState() => ConversationListState();
+}
+
+class ConversationListState extends State<ConversationList> {
+  late bool isTextMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    isTextMessage = widget.conversation.messageType == MessageType.text;
+    return Column(
+      children: [
+        const CircleAvatar(
+          radius: 38,
+          backgroundColor: Colors.blue,
+        ),
+        Card(
+            child: Row(
+          children: [
+            SizedBox(
+              width: isTextMessage ? 12 : 0,
+            ),
+            Expanded(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                  const SizedBox(height: 7),
+                  Center(
+                    child: Text(
+                      "${widget.conversation.from} ${isTextMessage ? "said" : "sent"} on ${formatDate(widget.conversation.time, [
+                            MM,
+                            ' ',
+                            d,
+                            ' at ',
+                            HH,
+                            ':',
+                            nn
+                          ])} ",
+                    ),
+                  ),
+                  const SizedBox(height: 7),
+                  isTextMessage
+                      ? SelectableText(
+                          widget.conversation.message!,
+                          textScaleFactor: 1.5,
+                        )
+                      : Container(
+                          alignment: Alignment.topCenter,
+                          child: widget.conversation.mediaLocation ==
+                                      MediaLocation.network ||
+                                  kIsWeb
+                              ? Image.network(widget.conversation.fileName!,
+                                  fit: BoxFit.fill)
+                              : Image.file(
+                                  fit: BoxFit.fill,
+                                  File(widget.conversation.fileName!)),
+                        ),
+                  const SizedBox(height: 14),
+                ]))
+          ],
+        ))
+      ],
+    );
+  }
+}

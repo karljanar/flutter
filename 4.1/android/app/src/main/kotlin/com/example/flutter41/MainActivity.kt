9e348231e28942e6df67a1955852eee285e1f38f
karljanar <karljanar@pop-os.localdomain>
2023-03-12 16:00:57 +0200

    4.1

---
+++ 4.1/android/app/src/main/kotlin/com/example/flutter41/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter41
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 4.1/lib/screens/chats.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({Key? key}) : super(key: key);
+
+  @override
+  ChatsScreenState createState() => ChatsScreenState();
+}
+
+class ChatsScreenState extends State<ChatsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("CHATS");
+  }
+}
